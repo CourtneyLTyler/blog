5337b3a5ea226b570275fda05f5fd5a153410f32
Unicourt1 <courtneytyler11@yahoo.com>
2018-04-23 16:15:48 -0400

    4.3 orders controller created

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
 #  root 'welcome#index'
 
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 end
