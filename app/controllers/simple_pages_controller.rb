3c2dbfb669f1d1f9a5e93faaf943c321d2d61bad
Unicourt1 <courtneytyler11@yahoo.com>
2018-04-25 00:15:50 -0400

    4.5 img carousel, product partial on Products page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,11 @@
 class SimplePagesController < ApplicationController
+
   def index
+    @products = Product.all
   end
 
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
-
+  
 end
