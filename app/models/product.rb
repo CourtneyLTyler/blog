a2e775acab26130b93437511231a1d08441e3e46
Unicourt1 <courtneytyler11@yahoo.com>
2018-08-15 17:58:41 -0400

    5.8 stars not showing

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,11 @@ class Product < ApplicationRecord
 
   def lowest_rating_comment
     comments.rating_asc.first
-  end 
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
 
   def self.search(search_term)
     if(Rails.env.production?)
