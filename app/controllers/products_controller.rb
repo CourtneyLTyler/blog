c6db73aa49c0b916614b49fe07b941593d4f6d32
Unicourt1 <courtneytyler11@yahoo.com>
2018-08-31 18:52:20 -0400

    5.10 fix comments and style

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,9 +26,11 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC")
-    @comments = Comment.all.paginate(page: params[:page], per_page: 5)
+      @product = Product.find(params[:id])
+      @comments = @product.comments.paginate(:page => params[:page], :per_page => 3).order("created_at DESC")
   end
+
+
 
   # GET /products/new
   def new
