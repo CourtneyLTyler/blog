6d75250ccf9d5887bd7cdc5e1cb8ea82fa225a09
Unicourt1 <courtneytyler11@yahoo.com>
2018-09-26 04:25:48 -0400

    6.7 add byebug gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
 
   rescue_from CanCan::AccessDenied do |exception|
+    # Rails.logger.debug "Access Denied: #{exception.message}"
     redirect_to main_app.root_url, alert: exception.message
   end
 
